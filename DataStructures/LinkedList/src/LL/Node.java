1303dee448d1c025aa5cc8761d5d7595379e5fd9
pratugup <pratugup>
2018-04-14 11:07:47 +0530

    Adding Linked List

---
+++ DataStructures/LinkedList/src/LL/Node.java
@@ -0,0 +1,6 @@
+package LL;
+
+public class Node {
+    int data;
+    Node next;
+}
---
+++ DataStructures/LinkedList/src/LL/run.java
@@ -0,0 +1,12 @@
+package LL;
+
+public class run {
+    public static void main(String[] srgs) {
+        LinkedList list = new LinkedList();
+        list.insert(5);
+        list.insert(6);
+        list.show();
+        list.insertAtStart(10);
+        list.show();
+    }
+}
